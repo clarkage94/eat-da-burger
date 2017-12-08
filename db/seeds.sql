18127c5875317995b28b82ddfdf9b50892f4aabd
Sean Clark <seanclark@Seans-MBP.fios-router.home>
2017-12-07 23:04:01 -0500

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,9 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("Double Bacon Cheeseburger", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("50/50 Cheeseburger", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Glupburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("In-N-Out Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Burger", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Angus Burger", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Chili Burger", true);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Pepper Jack Cheese Turkey Burger", true);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Gluten Free Bacon Cheeseburger", true);
-
